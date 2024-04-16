3dcdbe719a9d5b9f30547353554263c30ea1f467
Tim Baker <71035614+stltimbaker@users.noreply.github.com>
2024-04-16 10:14:26 -0500

    Component Store done

--- demo/src/app/products/products-page/products-page.component.ts
+++ demo/src/app/products/products-page/products-page.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 
-import { Store } from '@ngrx/store';
+import { Store, provideStore } from '@ngrx/store';
 import { ProductsPageActions } from '../state/products.actions';
 import { selectProducts, selectProductsErrorMessage, selectProductsLoading, selectProductsShowProductCode, selectProductsTotal } from '../state/products.selectors';
+import { ProductsStore } from '../state/products.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore]
 })
 export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+  // products$ = this.store.select(selectProducts);
+  products$ = this.productsStore.products$;
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
   showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {
+  constructor(private store: Store,
+    private productsStore: ProductsStore) {
     this.store.subscribe((store) => console.log(store));
+  } 
+
+  ngOnInit() {
+    this.productsStore.getProducts();
   }
 
   toggleShowProductCode() {
